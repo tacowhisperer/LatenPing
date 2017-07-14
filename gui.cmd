92ba041c9b135ea649b86d2b5f43f02e9ba4e2cb
tacowhisperer <chato94@gmail.com>
2017-07-13 19:21:14 -0500

    Added starting logic to GUI

--- gui.cmd
+++ gui.cmd
@@ -1,1 +1,21 @@
 @echo off
+REM    ************************************************************************
+	:: gui.cmd
+	::
+	::     This batch file uses native Windows CMD.EXE pure-batch commands to
+	::     set a framework with which messages may be displayed to the console.
+	::
+	:: USAGE:
+	::     Take relevant framework code and insert custom code where applicable
+REM    ************************************************************************
+
+REM Hold the options that are read by the updateuserinterface function 
+	:: Initial window width and height values
+	set WIDTH=25
+	set HEIGHT=5
+
+	:: Padding values to add to the top and bottom of messages displayed.
+	set PADDING_TB=0
+	set PADDING_LR=0
+:: Arguments - <line1> <line2> <line3> ... <lineN>
+:updateuserinterface
